9aeb70dd22beb204b68443b4a00d22a3c881612d
Raphael <rapha23_leon@hotmail.com>
2025-01-16 09:41:43 -0300

    Alteração: Hello Aline

---
+++ how 35706c2bec26c915b6b3c19bb3713922eeaabef4aula9.py
@@ -0,0 +1,11 @@
+[33mcommit 35706c2bec26c915b6b3c19bb3713922eeaabef4[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: Raphael <rapha23_leon@hotmail.com>
+Date:   Thu Jan 16 09:23:20 2025 -0300
+
+    Adicionado arquivo python com nova linha
+
+[33mcommit f2e1ee21400ad3a719b2339c859f9dfdb7b81c9e[m
+Author: Raphael <rapha23_leon@hotmail.com>
+Date:   Thu Jan 16 09:15:50 2025 -0300
+
+    Primeiro commit com os arquivos iniciais
